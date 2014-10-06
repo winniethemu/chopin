121d4e4b5f60ac4ec604ba75b9c739ca9c23c4b0
Mu Ye <emailyemu@gmail.com>
2014-10-06 01:22:37 -0400

    Getting the location data of a post quick and dirty.

--- nocturne.py
+++ nocturne.py
@@ -14,6 +14,20 @@ api = InstagramAPI(
 
 @app.route('/')
 def index():
+    # Retrieve geo location of a post
+    user = api.user_search('foodmaap')[0]
+    user_id = user.id
+    accounts = api.user_follows(user_id)[0]
+
+    posts = list()
+    for account in accounts:
+        posts += list(api.user_recent_media(user_id=account.id, count=10))
+    posts = posts[0]
+
+    post = posts[0]
+    latitude = post.location.point.latitude
+    longitude = post.location.point.longitude
+
     return render_template('nocturne.html')
 
 if __name__ == '__main__':
