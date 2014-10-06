9336e50116b0b044b7d6e0128a0c4e36a4d48a58
Mu Ye <emailyemu@gmail.com>
2014-10-05 22:48:15 -0400

    First commit.

---
+++ nocturne.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.config.from_envvar('NOCTURNE_SETTINGS', silent=True)
+
+@app.route('/')
+def index():
+    return render_template('nocturne.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
