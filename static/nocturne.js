c8e56f628ea90e6d0226d6cfefd53b75803b7fca
Mu Ye <emailyemu@gmail.com>
2014-10-26 00:48:56 -0400

    Use Leaflet API.

--- static/nocturne.js
+++ static/nocturne.js
@@ -1,5 +1,3 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoiZm9vZG1hYXAiLCJhIjoid1VTdVZTdyJ9.ObzX5yN1zDpYjsJGrCK35g';
-
 var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik/{z}/{x}/{y}.png', {
   attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
 });
@@ -7,3 +5,9 @@ var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik
 var map = L.map('map')
             .addLayer(mapboxTiles)
             .setView([42.3610, -71.0587], 15);
+
+function onMapClick(e) {
+      alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
