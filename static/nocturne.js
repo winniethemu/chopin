4157892b586ed4c40d64eaef9eebec40d3023dc3
Mu Ye <emailyemu@gmail.com>
2014-10-26 01:28:58 -0400

    Default to Toronto.

--- static/nocturne.js
+++ static/nocturne.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([42.3610, -71.0587], 15);
+var TORONTO = [43.6520, -79.3900];
+
+var map = L.map('map').setView(TORONTO, 14);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik/{z}/{x}/{y}.png', {
   attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
