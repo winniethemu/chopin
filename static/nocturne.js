2537ef2e8b13ba75929c923cc3d47b1c0ea45a4b
Mu Ye <emailyemu@gmail.com>
2014-10-26 00:54:18 -0400

    Refactor.

--- static/nocturne.js
+++ static/nocturne.js
@@ -1,13 +1,5 @@
-var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik/{z}/{x}/{y}.png', {
+var map = L.map('map').setView([42.3610, -71.0587], 15);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik/{z}/{x}/{y}.png', {
   attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-});
-
-var map = L.map('map')
-            .addLayer(mapboxTiles)
-            .setView([42.3610, -71.0587], 15);
-
-function onMapClick(e) {
-      alert("You clicked the map at " + e.latlng);
-}
-
-map.on('click', onMapClick);
+}).addTo(map);
