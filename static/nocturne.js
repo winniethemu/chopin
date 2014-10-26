c637738fadc07a345f74e3b1478d84509ebf1227
Mu Ye <emailyemu@gmail.com>
2014-10-26 00:10:00 -0400

    Load a map.

--- static/nocturne.js
+++ static/nocturne.js
@@ -1,1 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+L.mapbox.accessToken = 'pk.eyJ1IjoiZm9vZG1hYXAiLCJhIjoid1VTdVZTdyJ9.ObzX5yN1zDpYjsJGrCK35g';
+
+var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik/{z}/{x}/{y}.png', {
+  attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+});
+
+var map = L.map('map')
+            .addLayer(mapboxTiles)
+            .setView([42.3610, -71.0587], 15);
