91cde65610adaa82ad9172c908701a606dd7ec14
Mu Ye <emailyemu@gmail.com>
2014-11-01 20:03:08 -0400

    Basic popup code. Use Bower for asset management.

--- static/nocturne.js
+++ static/nocturne.js
@@ -1,4 +1,7 @@
 $(function() {
+  /*
+   * Initialization
+   */
   var TORONTO = [43.6520, -79.3900];
   var map = L.map('map').setView(TORONTO, 14);
 
@@ -8,13 +11,23 @@ $(function() {
                  'target="_blank">Terms &amp; Feedback</a>'
   }).addTo(map);
 
+  /*
+   * Main API call
+   */
   $.ajax({
     url: 'api/v1/locations',
     success: function(response) {
       for (var i = 0; i < response.data.length; i++) {
-        var location = response.data[i];
-        L.marker([location.latitude, location.longitude]).addTo(map);
+        var place = response.data[i];
+        var pin = L.marker([place.latitude, place.longitude]).addTo(map);
+        pin
+          .bindPopup("<b>Hello world!</b><br>I am a popup.")
+          .openPopup();
       }
     }
   });
+
+  /*
+   * Helpers
+   */
 });
