9a67ab95ec91542d5669995c8a3bc8b470b323bb
Mu Ye <emailyemu@gmail.com>
2014-11-16 17:00:53 -0500

    Click event.

--- assets/nocturne.js
+++ assets/nocturne.js
@@ -1,3 +1,13 @@
+var NocturneMarker = L.Marker.extend({
+  options: {
+    image_url: '',
+    like_count: 0,
+    link: '',
+    name: '',
+    recent_posts: []
+  }
+});
+
 $(function() {
   /*
    * Initialization
@@ -5,20 +15,8 @@ $(function() {
   var TORONTO = [43.6500, -79.3900];
   var map = L.map('map').setView(TORONTO, 14);
   var detailsTmpl = doT.template($('#location-details').html());
-  var NocturneMarker = L.Marker.extend({
-    options: {
-      image_url: '',
-      like_count: 0,
-      link: '',
-      name: '',
-      recent_posts: []
-    }
-  });
 
-  // The images are just a copy of the ones in bower_components
-  // because Flask + Bower is lolz
   L.Icon.Default.imagePath = 'static/images';
-
   L.tileLayer(
     'https://{s}.tiles.mapbox.com/v3/foodmaap.jpid9gik/{z}/{x}/{y}.png', {
     attribution: '<a href="http://www.mapbox.com/about/maps/" ' +
@@ -33,6 +31,7 @@ $(function() {
     success: function(response) {
       for (var i = 0; i < response.data.length; i++) {
         var place = response.data[i];
+
         var pin = new NocturneMarker([place.latitude, place.longitude], {
             image_url: place.image_url,
             like_count: place.like_count,
@@ -40,6 +39,8 @@ $(function() {
             name: place.name,
             recent_posts: place.recent_posts
         }).addTo(map);
+
+        pin.on('click', showPlaceDetails);
       }
     }
   });
@@ -47,4 +48,7 @@ $(function() {
   /*
    * Helpers
    */
+  function showPlaceDetails(e) {
+    var details = e.target.options;
+  }
 });
